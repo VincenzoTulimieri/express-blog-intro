0f1f521bcbf5bb3466a191b466218dd9c7c02b3c
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-26 21:02:30 +0100

    fix: aggiustato collegamento per visibilità delle foto

--- menu.js
+++ menu.js
@@ -2,31 +2,31 @@ const menu =[
     ciambellone = {
         titolo: 'ciambellone',
         contenuto:'',
-        immagine:'img/ciambellone.jpeg',
+        immagine:'pubblic/img/ciambellone.jpeg',
         tags: []
     },
     cracker = {
         titolo: 'cracker di barbabietola',
         contenuto:'',
-        immagine:'img/cracker_barbabietola.jpeg',
+        immagine:'pubblic/img/cracker_barbabietola.jpeg',
         tags: []
     },
     paneFritto = {
         titolo: 'Pane fritto dolce',
         contenuto:'',
-        immagine:'img/pane_fritto_dolce.jpeg',
+        immagine:'pubblic/img/pane_fritto_dolce.jpeg',
         tags: []
     },
     pastaBarbabietola = {
         titolo: 'Pane di Barbabietola',
         contenuto:'',
-        immagine:'img/pasta_barbabietola.jpeg',
+        immagine:'pubblic/img/pasta_barbabietola.jpeg',
         tags: []
     },
     tortaPaesana = {
-        titolo: 'torta paesana',
+        titolo: 'Torta Paesana',
         contenuto:'',
-        immagine:'img/torta_paesana.jpeg',
+        immagine:'pubblic/img/torta_paesana.jpeg',
         tags: []
     },
 ]
