8ba653cd0070f0ef17a97c3fa5e457a6ddcd82ec
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-26 20:48:03 +0100

    feat: creato array con dnetro elementi del menu

---
+++ menu.js
@@ -0,0 +1,37 @@
+const menu =[
+    ciambellone = {
+        titolo: 'ciambellone',
+        contenuto:'',
+        immagine:'img/ciambellone.jpeg',
+        tags: []
+    },
+    cracker = {
+        titolo: 'cracker di barbabietola',
+        contenuto:'',
+        immagine:'img/cracker_barbabietola.jpeg',
+        tags: []
+    },
+    paneFritto = {
+        titolo: 'Pane fritto dolce',
+        contenuto:'',
+        immagine:'img/pane_fritto_dolce.jpeg',
+        tags: []
+    },
+    pastaBarbabietola = {
+        titolo: 'Pane di Barbabietola',
+        contenuto:'',
+        immagine:'img/pasta_barbabietola.jpeg',
+        tags: []
+    },
+    tortaPaesana = {
+        titolo: 'torta paesana',
+        contenuto:'',
+        immagine:'img/torta_paesana.jpeg',
+        tags: []
+    },
+]
+
+module.exports = menu
+
+
+
